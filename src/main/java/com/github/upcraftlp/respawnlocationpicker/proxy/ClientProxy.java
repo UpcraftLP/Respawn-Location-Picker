6b0fba8733996c82f83213cec6d523d68fff533b
UpcraftLP <j-schirra@gmx.de>
2018-01-21 01:09:30 +0100

    credits

--- src/main/java/com/github/upcraftlp/respawnlocationpicker/proxy/ClientProxy.java
+++ src/main/java/com/github/upcraftlp/respawnlocationpicker/proxy/ClientProxy.java
@@ -1,9 +1,17 @@
 package com.github.upcraftlp.respawnlocationpicker.proxy;
 
+import com.github.upcraftlp.respawnlocationpicker.Main;
+import com.github.upcraftlp.respawnlocationpicker.Reference;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
 
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+        Main.metaData.credits = Reference.CREDITS;
+    }
 }
